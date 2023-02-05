c00bf9f41fca1b49695a62aecb315872b2959a9d
Jim Lao <laojimmarklino@gmail.com>
2023-02-05 16:26:42 +0800

    added product model and migration with data seed dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +21,32 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Product::create(
+            [
+                'generic_name' => 'Filgrastim',
+                'brand_name' => 'Macroleuco',
+                'product_form' => '300mcg, vial box of 10',
+                'market_price' => 2100.00
+            ]
+        );
+
+        Product::create(
+            [
+                'generic_name' => 'Filgrastim',
+                'brand_name' => 'Filgra',
+                'product_form' => '300mcg, Pre-filled Syringe box of 2',
+                'market_price' => 1500.00
+            ]
+        );
+
+        Product::create(
+            [
+                'generic_name' => 'Doxorubicin',
+                'brand_name' => 'Doxoruba',
+                'product_form' => '25mg/ml solution',
+                'market_price' => 1500.00
+            ]
+        );
     }
 }
