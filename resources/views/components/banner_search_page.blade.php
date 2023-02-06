ada21b1f82937d67e4afe053b19587a82fdaf444
gitlao93 <laojimmarklino@gmail.com>
2023-02-06 13:25:07 +0800

    added a header and hero section

---
+++ resources/views/components/banner_search_page.blade.php
@@ -0,0 +1,16 @@
+<section class="hero-banner">
+    <div class="vh-60 main-content">
+        <div class="flex-col fl-align-center fl-justify-content-center h-100">
+            <h1>De Oro Mediko Drug<br>Distribution services</h1>
+            <p class="wd-50">Welcome to <b>De Oro Mediko Drug Distribution Services</b>, your trusted partner in providing medical drugs to government agencies. We are dedicated to delivering high-quality and affordable drugs to those in need.</p>
+            <form class="search-box">
+                <div class="input-box">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                    <input type="text" placeholder="Mag search ng gamot..." />
+                    <button class="button">Search</button>
+                    </div>
+            </form>
+        </div>
+        
+    </div>
+</section>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<header class="flex-row fl-align-center wd-100 pd-x-5 header-cont">
+       
+    <div class="flex-row fl-align-center wd-70 logo-container pd-y-3">
+        <img class="logo" src="{{asset('/images/logo.png')}}" alt="image logo"/>
+        <p>De Oro Mediko<br>Drug Distribution Service</p>
+    </div>
+    <div class="flex-row fl-justify-content-center wd-30">
+        <a href="tel:09172052719" class="contact-btn">Contact Us</a>
+    </div>
+</header>
