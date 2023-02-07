ea05b97a8b7a7e1039c0585008c10348a9c772b0
Mark Achacoso <makmakachacoso@gmail.com>
2023-02-07 16:38:15 +0800

    added create products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -22,5 +22,35 @@ class ProductController extends Controller
         return view('products',['productLists' => Product::all()]);
     }
 
+    public function create() {
+        return view('add-prod');
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'generic_name' => 'required',
+            'brand_name' => 'required',
+            'product_form' => 'required',
+            'market_price' => 'required',
+            'image_path' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+   
+        $input = $request->all();
+   
+        if ($image = $request->file('image_path')) {
+            $destinationPath = 'images/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image_path'] = "$profileImage";
+        }
+     
+        Product::create($input);
+      
+        // return redirect()->route('/products')
+        //                 ->with('success','Product created successfully.');
+    }
+
     
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,10 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'generic_name', 'brand_name', 'product_form', 'market_price', 'image_path'
+    ];
+
     public function scopeFilter($query, array $filters){
         if($filters['search'] ?? false) {
             $query->where('generic_name', 'like', '%' . $filters['search'] . '%')
