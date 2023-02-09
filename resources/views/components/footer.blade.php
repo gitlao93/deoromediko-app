73d1e386e3084af03791de21b5192e33da14507b
gitlao93 <laojimmarklino@gmail.com>
2023-02-09 15:06:57 +0800

    completed the cleaning of css for the landing and product page and add responsiveness

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -84,24 +84,23 @@
         <ul>
         <li>
             <h6>Open Hours</h6>
-            <div class="">
+            <div class="footer-info-wrapper">
                 <i class="fa-solid fa-clock"></i>
                 <div>
-                    <p>Monday To Friday</p>
-                    <p>9:00 am to 5:00 pm</p>
+                    <p>Monday To Friday<br>9:00 am to 5:00 pm</p>
                 </div>
 
             </div>
         </li>
         <li>
             <h6>Contact us</h6>
-            <div class="">
+            <div class="footer-info-wrapper">
                 <i class="fa fa-phone" aria-hidden="true"></i>
                 <a href="tel:09172052719">09172052719</a>
             </div>
         <li>
             <h6>Visit Us At</h6>
-            <div class="">
+            <div class="footer-info-wrapper">
                 <a target="_blank" href="https://twitter.com/deoromedikodrug"><i
                         class="fa-brands fa-facebook-f"></i></a>
                 <a target="_blank" href="https://twitter.com/deoromedikodrug"><i class="fa-brands fa-twitter"></i></a>
@@ -115,7 +114,7 @@
             <h6>Location</h6>
             <p>G/F Laourse Pension House, ilustre street. barangay 4-a Davao city</p>
             <div class="">
-                <div class="gmap_canvas"><iframe width="400" height="400" id="gmap_canvas"
+                <div class="gmap_canvas"><iframe id="gmap_canvas"
                         src="https://maps.google.com/maps?q=G/F%20Laourse%20Pension%20House,%20ilustre%20street&t=&z=13&ie=UTF8&iwloc=&output=embed"
                         frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>
             </div>
