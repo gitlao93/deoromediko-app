4f577b5a828e13f19138de27ca15e49b7190bb0f
Mark Achacoso <makmakachacoso@gmail.com>
2023-02-10 13:27:27 +0800

    edit image upload

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -13,7 +13,7 @@
                         <img src="{{ $list->image_path != null ? asset('/images/' . $list->image_path) : asset('/images/no-photo-available1350441335.png') }}"
                             alt="Product_image" class="img-in-card ">
                         <h3 style="color: #000;">{{ $list->generic_name }}</h3>
-                        <p>{{ number_format($list->market_price, 2) }}/Bottle</p>
+                        <p>&#8369;{{ number_format($list->market_price, 2) }}/Bottle</p>
                     </a>
                 </div>
             @endforeach
