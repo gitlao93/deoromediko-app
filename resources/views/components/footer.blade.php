e161049b66b13fe5cb4e50a3d6894477f9dc0a29
gitlao93 <laojimmarklino@gmail.com>
2023-02-11 19:04:29 +0800

    cleaned

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,82 +1,8 @@
-{{-- <footer class="footer-section">
-    <div class="container footer-content flex-row fl-justify-content-center fl-align-start" >
-        <div class="wd-30 ">
-            <div class="flex-row fl-justify-content-start fl-align-center logo-container">
-                <img src="{{asset('/images/logo.png')}}" alt="image logo"/>
-                <p>De Oro Mediko<br>Drug Distribution Service</p>
-            </div>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae ad, odit doloremque illo sapiente nostrum 
-                enim minima incidunt nesciunt culpa ex mollitia illum esse ea modi magnam nam sequi deleniti.
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga doloribus quidem distinctio asperiores odio assumenda illo deleniti, officiis, 
-            </p>
-        </div>
-        
-        <div class="footer-content footer-list wd-20">
-         
-          <ul>
-            <li>
-                <h6>Open Hours</h6>
-                <div class="icon-details">
-                    <i class="fa-solid fa-clock"></i>
-                    <div>
-                        <p>Monday To Friday</p>
-                        <p>9:00 am to 5:00 pm</p>
-                    </div>
-
-                </div>
-            </li>
-          </ul>
-        </div>
-
-        <div class="footer-content footer-list wd-30 flex-row fl-justify-content-center">
-         
-          <ul class="wd-80">
-            <li>
-                <h6>Open Hours</h6>
-                <div class="icon-details">
-                    <i class="fa-solid fa-clock"></i>
-                    <div>
-                        <p>Monday To Friday</p>
-                        <p>9:00 am to 5:00 pm</p>
-                    </div>
-
-                </div>
-            </li>
-            <li>
-                <h6>Contact us</h6>
-                <div class="icon-details">
-                    <i class="fa fa-phone" aria-hidden="true"></i>
-                    <a href="tel:09172052719">09172052719</a>
-                </div>
-            <li>
-                <h6>Visit Us At</h6>
-                <div class="icon-details">
-                    <a target="_blank" href="https://twitter.com/deoromedikodrug"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a target="_blank" href="https://twitter.com/deoromedikodrug"><i class="fa-brands fa-twitter"></i></a>
-                </div>
-            </li>
-          </ul>
-        </div>
-        <div class="footer-content wd-30">
-            <div class="footer-details">
-                <h6>Location</h6>
-                <i></i>
-                <p>G/F Laourse Pension House, ilustre street. barangay 4-a Davao city</p>
-                <div class="mapouter"><div class="gmap_canvas"><iframe width="400" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=G/F%20Laourse%20Pension%20House,%20ilustre%20street&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div></div>
-            </div>
-          </div>
-        </div>
-</footer> --}}
-
 <footer class="footer-container">
 
     <div class="footer-logo-container">
         <div class="footer-logo">
-            <a href="/"><img class="logo" src="{{ asset('/images/logo.png') }}" alt="image logo" /></a>
-            <a href="/">
-                <p>De Oro Mediko<br>Drug Distribution Service</p>
-            </a>
+            <a href="/"><img class="logo" src="{{ asset('/images/DeOroMedikoLogo.png') }}" alt="image logo" /></a>
         </div>
     </div>
 
